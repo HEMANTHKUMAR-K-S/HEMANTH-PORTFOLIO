a31c3e3f03f006b2216486acedc6ffcdfae9cfa9
hemanth <hemanthtarget8438@gmail.com>
2025-01-12 09:29:08 +0530

    refactor navbar functionality and enhance styling with new background images

--- script.js
+++ script.js
@@ -1,15 +1,38 @@
-
 const toggleIcon = document.getElementById('menu-toggle');
 const navBar = document.getElementById('navbar');
 let isOpen = false;
 
-toggleIcon.addEventListener('click', () => {
-if (isOpen) {
-    navBar.style.display = 'none';
-    toggleIcon.innerHTML = '<i class="fas fa-bars"></i>';
-} else {
+toggleIcon.addEventListener('click', (event) => {
+    event.stopPropagation();
+    if (isOpen) {
+        closeNavBar();
+    } else {
+        openNavBar();
+    }
+});
+
+document.addEventListener('click', () => {
+    if (isOpen) {
+        closeNavBar();
+    }
+});
+
+document.addEventListener('visibilitychange', () => {
+    if (document.hidden) {
+        closeNavBar();
+    }
+});
+
+function openNavBar() {
     navBar.style.display = 'block';
     toggleIcon.innerHTML = '<i class="fas fa-times"></i>';
+    document.body.style.overflow = 'hidden';
+    isOpen = true;
 }
-isOpen = !isOpen;
-});
+
+function closeNavBar() {
+    navBar.style.display = 'none';
+    toggleIcon.innerHTML = '<i class="fas fa-bars"></i>';
+    document.body.style.overflow = 'auto';
+    isOpen = false;
+}
